b2125f51274d7e7322d69cb533085c110851af2c
Mark Bennison <markbennison@users.noreply.github.com>
2023-09-01 11:48:04 +0100

    Added Materials

--- Assets/Player/Scripts/PlayerLocomotion.cs
+++ Assets/Player/Scripts/PlayerLocomotion.cs
@@ -1,14 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerLocomotion : MonoBehaviour
 {
 
-    private TestSteeringSetup inputActions;
-    private InputAction move;
+    TestSteeringSetup inputActions;
+    InputAction move;
+    InputAction look;
+
+    CharacterController characterController;
+    public Transform cameraContainer;
+    Transform playerContainer;
+
+    public float speed = 6.0f;
+    //public float jumpSpeed = 10f;
+    public float mouseSensitivity = 2f;
+    public float gravity = 20.0f;
+    public float lookUpClamp = -30f;
+    public float lookDownClamp = 60f;
+
+    Vector3 moveDirection = Vector3.zero;
+    float rotateX, rotateY;
 
     private void Awake()
     {
@@ -16,9 +33,10 @@ public class PlayerLocomotion : MonoBehaviour
     }
 
     void Start()
-
     {
-        
+        Cursor.visible = false;
+        characterController = GetComponent<CharacterController>();
+
     }
 
     void Update()
@@ -28,13 +46,19 @@ public class PlayerLocomotion : MonoBehaviour
 
     void FixedUpdate()
     {
-        Debug.Log("Move: " + move.ReadValue<Vector2>());
+        //Debug.Log("Move: " + move.ReadValue<Vector2>());
+
+        Locomotion();
+        RotateAndLook();
     }
 
     private void OnEnable()
     {
         move = inputActions.Player.Move;
         move.Enable();
+
+        look = inputActions.Player.Look;
+        look.Enable();
 
         inputActions.Player.Fire.performed += Fire;
         inputActions.Player.Fire.Enable();
@@ -43,6 +67,7 @@ public class PlayerLocomotion : MonoBehaviour
     private void OnDisable()
     {
         move.Disable();
+        look.Disable();
         inputActions.Player.Fire.Disable();
     }
 
@@ -51,4 +76,30 @@ public class PlayerLocomotion : MonoBehaviour
         throw new NotImplementedException();
     }
 
+    void Locomotion()
+    {
+        if (characterController.isGrounded) // When grounded, set y-axis to zero (to ignore it)
+        {
+            Vector2 moveInput = move.ReadValue<Vector2>();
+            moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void RotateAndLook()
+    {
+        Vector2 lookInput = look.ReadValue<Vector2>();
+        rotateX = lookInput.x * mouseSensitivity;
+        rotateY -= lookInput.y * mouseSensitivity;
+
+        rotateY = Mathf.Clamp(rotateY, lookUpClamp, lookDownClamp);
+
+        cameraContainer.transform.Rotate(0f, rotateX, 0f);
+
+        cameraContainer.transform.localRotation = Quaternion.Euler(rotateY, 0f, 0f);
+    }
 }
